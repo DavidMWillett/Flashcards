38e1dd360819781ee8b31bc748400200cf33bd00
david <david.m.willett@gmail.com>
2020-06-03 10:20:30 +0100

    Program adapted to add main loop and "add", "ask" and "exit" functions.

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -1,47 +1,63 @@
 package flashcards
 
+import kotlin.random.Random
+
 fun main() {
-    showCards(createCards())
+    loop@ do {
+        println("Input the action (add, remove, import, export, ask, exit):")
+        when (readLine()!!) {
+            "add" -> Deck.add()
+            "ask" -> Deck.ask()
+            "exit" -> {
+                println("Bye bye!")
+                break@loop
+            }
+        }
+    } while (true)
 }
 
-fun createCards(): Map<String, String> {
-    println("Input the number of cards:")
-    val numCards = readLine()!!.toInt()
-    val cards = mutableMapOf<String, String>()
-    repeat(numCards) {
-        println("The card #${it + 1}:")
-        var term = readLine()!!
-        while (cards.containsKey(term)) {
-            println("The card \"$term\" already exists. Try again:")
-            term = readLine()!!
+object Deck {
+    private val cards = mutableMapOf<String, String>()
+
+    private fun <K, V> Map<K, V>.random(): Map.Entry<K, V> = entries.elementAt(Random.nextInt(size))
+
+    private fun <K, V> Map<K, V>.getKey(value: V): K = filterValues { it == value }.keys.single()
+
+    fun add() {
+        println("The card:")
+        val term = readLine()!!
+        if (cards.containsKey(term)) {
+            println("The card \"$term\" already exists")
+            return
         }
-        println("The definition of the card #${it + 1}:")
-        var definition = readLine()!!
-        while (cards.containsValue(definition)) {
-            println("The definition \"$definition\" already exists. Try again:")
-            definition = readLine()!!
+        println("The definition of the card:")
+        val definition = readLine()!!
+        if (cards.containsValue(definition)) {
+            println("The definition \"$definition\" already exists.")
+            return
         }
         cards[term] = definition
+        println("The pair (\"$term\":\"$definition\") has been added.")
+        println()
     }
-    return cards
-}
 
-fun showCards(cards: Map<String, String>) {
-    cards.forEach {
-        println("Print the definition of \"${it.key}\":")
-        val answer = readLine()!!
-        if (answer == it.value) {
-            println("Correct answer.")
-        } else {
-            print("Wrong answer. The correct one is \"${it.value}\"")
-            if (cards.containsValue(answer)) {
-                val definition = cardByValue(cards, answer)
-                print(", you've just written the definition of \"$definition\"")
+    fun ask() {
+        println("How many times to ask?")
+        val times = readLine()!!.toInt()
+        repeat(times) {
+            val card = cards.random()
+            println("Print the definition of \"${card.key}\":")
+            val answer = readLine()!!
+            if (answer == card.value) {
+                println("Correct answer.")
+            } else {
+                print("Wrong answer. The correct one is \"${card.value}\"")
+                if (cards.containsValue(answer)) {
+                    val definition = cards.getKey(answer)
+                    print(", you've just written the definition of \"$definition\"")
+                }
+                println(".")
             }
-            println(".")
         }
     }
 }
-
-fun cardByValue(cards: Map<String, String>, value: String) =
-        cards.filterValues { it == value }.keys.single()
